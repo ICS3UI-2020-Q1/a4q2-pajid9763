33d6e006b29fda908f8c3fd88fa186616621c963
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 14:58:20 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * program that needs negative integer to quit
+ * 
+ * @author Darian
  */
 public class Main {
 
@@ -9,7 +12,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+  Scanner input = new Scanner(System.in);
+
+  // declare the number variable 
+  int number;
+  // ask the user for a number
+  System.out.println("Please enter a positive integer. Enter a negative integer to quit.");
+  
+  do {
+    // get the user's number 
+    number = input.nextInt();
+
+    // check whether the number is positive or negative
+    if (number > 0){
+      System.out.println("Please enter a positive integer. Enter a negative integer to quit.");
+  
+    } else if (number < 0){
+      System.out.println("All done!");
+      break;
     
+    } 
+    }while (number > 0);
   }
 }
+  
